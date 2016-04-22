6a0a8cad3ef3dd81c5ebe2964f66a63de418b57e
LeoTianJLL <Leo.Tian@ap.jll.com>
2016-04-22 15:59:17 +0800

    Done

--- ChatChat/ChatViewController.swift
+++ ChatChat/ChatViewController.swift
@@ -29,6 +29,23 @@ class ChatViewController: JSQMessagesViewController {
   
     // MARK: Properties
     var messages = [JSQMessage]()
+    var userIsTypingRef: Firebase! // 1 Create a reference that tracks whether the local user is typing.
+    private var localTyping = false // 2 Store whether the local user is typing in a private property.
+    var isTyping: Bool {
+        
+        get {
+            return localTyping
+        }
+        set {
+            
+            // 3 Using a computed property, you can update userIsTypingRef each time you update this property.
+            localTyping = newValue
+            userIsTypingRef.setValue(newValue)
+            
+        }
+        
+    }
+    var usersTypingQuery: FQuery!
     
     let rootRef = Firebase(url: "https://emily-realtime-chat.firebaseio.com/")
     var messageRef: Firebase!
@@ -47,19 +64,10 @@ class ChatViewController: JSQMessagesViewController {
         messageRef = rootRef.childByAppendingPath("messages")
     }
     
-    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
-        
-        let itemRef = messageRef.childByAutoId() // 1 Using childByAutoId(), you create a child reference with a unique key.
-        
-        print("itemRef id = \(itemRef.key)")
-        
-        let messageItem = ["text": text, "senderId": senderId] // 2 Create a dictionary to represent the message. A [String: AnyObject] works as a JSON-like object.
-        
-        itemRef.setValue(messageItem) // 3 Save the value at the new child location.
-        
-        JSQSystemSoundPlayer.jsq_playMessageSentSound() // 4 Play the canonical “message sent” sound.
-        
-        finishSendingMessage() // 5 Complete the “send” action and reset the input toolbar to empty.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        observeMessages()
+        observeTyping()
     }
   
     override func viewDidDisappear(animated: Bool) {
@@ -106,6 +114,30 @@ class ChatViewController: JSQMessagesViewController {
         
         return cell
     }
+
+    override func didPressSendButton(button: UIButton!, withMessageText text: String!, senderId: String!, senderDisplayName: String!, date: NSDate!) {
+        
+        let itemRef = messageRef.childByAutoId() // 1 Using childByAutoId(), you create a child reference with a unique key.
+        
+        print("itemRef id = \(itemRef.key)")
+        
+        let messageItem = ["text": text, "senderId": senderId] // 2 Create a dictionary to represent the message. A [String: AnyObject] works as a JSON-like object.
+        
+        itemRef.setValue(messageItem) // 3 Save the value at the new child location.
+        
+        JSQSystemSoundPlayer.jsq_playMessageSentSound() // 4 Play the canonical “message sent” sound.
+        
+        finishSendingMessage() // 5 Complete the “send” action and reset the input toolbar to empty.
+        
+        isTyping = false
+    }
+    
+    override func textViewDidChange(textView:UITextView){
+        super.textViewDidChange(textView)
+        
+        isTyping = textView.text != ""
+    }
+    
     
     private func setupBubbles() {
         
@@ -115,6 +147,50 @@ class ChatViewController: JSQMessagesViewController {
         
     }
     
+    
+    private func observeMessages() {
+        
+        // 1 Start by creating a query that limits the synchronization to the last 25 messages.
+        let messagesQuery = messageRef.queryLimitedToLast(25)
+        
+        // 2 Use the .ChildAdded event to observe for every child item that has been added, and will be added, at the messages location.
+        messagesQuery.observeEventType(.ChildAdded) { (snapshot: FDataSnapshot!) in
+            
+            // 3 Extract the senderId and text from snapshot.value.
+            let id = snapshot.value["senderId"] as! String
+            let text = snapshot.value["text"] as! String
+            
+            // 4 Call addMessage() to add the new message to the data source.
+            self.addMessage(id, text: text)
+            
+            // 5 Inform JSQMessagesViewController that a message has been received.
+            self.finishReceivingMessage()
+            
+        }
+    }
+    
+    private func observeTyping() {
+        
+        let typingIndicatorRef = rootRef.childByAppendingPath("typingIndicator")
+        userIsTypingRef = typingIndicatorRef.childByAppendingPath(senderId)
+        userIsTypingRef.onDisconnectRemoveValue()
+        
+        // 1 You initialize the query by retrieving all users who are typing. This is basically saying, “Hey Firebase, go to the key /typingIndicators and get me all users for whom the value is true.”
+        usersTypingQuery = typingIndicatorRef.queryOrderedByValue().queryEqualToValue(true)
+        
+        // 2 Observe for changes using .Value; this will give you an update anytime anything changes.
+        usersTypingQuery.observeEventType(.Value) { (data: FDataSnapshot!) in
+        
+            // 3 You need to see how many users are in the query. If the there’s just one user, check to see if the local user is typing. If so, don’t display the indicator.
+            if data.childrenCount == 1 && self.isTyping {
+                return
+            }
+            
+            // 4 If there are more than zero users, and the local user isn’t typing, it’s safe to set the indicator. Call scrollToBottomAnimated(_:animated:) to ensure the indicator is displayed.
+            self.showTypingIndicator = data.childrenCount > 0
+            self.scrollToBottomAnimated(true)
+        }
+    }
     
     func addMessage(id:String, text:String){
         
